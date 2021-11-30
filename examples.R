702ce0b17b74d933f1343bbc4219184f7ad1f24f
Marium Tapal <mariumtapal@gmail.com>
2021-11-29 20:36:13 -0500

    clean code and run examples

---
+++ examples.R
@@ -0,0 +1,120 @@
+# load packages
+library(combinat)
+library(tidyverse)
+
+# source function from other script
+source("model.R")
+
+# vector of DNA species of different lengths
+species <- c(
+  "ATGCTTAGC",
+  "GTACCCAAT",
+  "GTACGTAAT",
+  "GTATAATA",
+  "GTACC",
+  "TACGGTA",
+  "GATCC",
+  "ATCG",
+  "ATTTGCA",
+  "ATCG"
+)
+
+# vector of stresses to applied to each dna
+stresses <- c(
+  "heat",
+  "light",
+  "light",
+  "food",
+  "heat",
+  "light",
+  "food",
+  NA,
+  "light",
+  "heat"
+)
+
+# merge vectors in a dataset
+data <- tibble(specie = species,
+               stress = stresses)
+
+# example 1: apply function to each row of data
+df <- map2_dfr(data$specie, data$stress, find_matches)
+
+# summary table
+summary <- df %>%
+  group_by(specie, stress) %>%
+  summarise(count_changes = n()) %>%
+  mutate(
+    total_nucleotides = nchar(specie),
+    prob_changes = count_changes / total_nucleotides
+  ) %>%
+  arrange(desc(prob_changes))
+
+# boxplot
+ggplot(summary, aes(x = prob_changes)) +
+  geom_boxplot()
+
+# scatterplot
+ggplot(summary, aes(x = total_nucleotides, y = prob_changes, 
+                    colour = stress, size = total_nucleotides)) +
+  geom_point()
+
+
+
+# try one specie with different stresses
+df2 <- map2_dfr("GTACGTAATA", c("food", "heat", "light"), find_matches)
+summary2 <- df2 %>%
+  group_by(specie, stress) %>%
+  summarise(count_changes = n()) %>%
+  mutate(
+    total_nucleotides = nchar(specie),
+    prob_changes = count_changes / total_nucleotides
+  ) %>%
+  arrange(desc(prob_changes))
+
+ggplot(summary, aes(x = prob_changes)) +
+  geom_boxplot()
+
+ggplot(summary2, aes(x = stress, y = prob_changes, fill = stress)) +
+  geom_col()
+
+
+# test all species in vector with all stresses
+
+# initiate empty tibble
+df3 <- tibble(
+  specie = character(),
+  new = character(),
+  stress = character(),
+  i = integer()
+)
+
+for (specie in species) {
+  df3 <- map2_dfr(specie, c("food", "heat", "light"), find_matches) %>% bind_rows(df3)
+}
+
+summary3 <- df3 %>%
+  group_by(specie, stress) %>%
+  summarise(count_changes = n()) %>%
+  mutate(
+    total_nucleotides = nchar(specie),
+    prob_changes = count_changes / total_nucleotides
+  ) %>%
+  arrange(desc(prob_changes))
+
+ggplot(summary3, aes(x = prob_changes)) +
+  geom_boxplot() +
+  facet_wrap(~stress)
+
+ggplot(summary3, aes(x = specie, y = prob_changes, fill = stress)) +
+  geom_col(position = "dodge") +
+  coord_flip()
+
+# avg percentage change by stress ~ about the same!
+summary3 %>% 
+  select(stress, prob_changes) %>% 
+  group_by(stress) %>% 
+  summarise(mean = mean(prob_changes))
+
+# in the real world this number would be different because we would use real DNA and mutated DNA
+# but this is a way to visualize the changes by stress
--- model.R
+++ model.R
@@ -1,65 +1,27 @@
 # stresses (what does each do?): heat, light, food given, -- maybe a slider of how much of each is given?
-
 # create a string DNA of the anemones -- we have six of them? or sth
-
 # right before death - change
 # some string didn't change -- resistant?
-
 # string length?
-
 # new string after stress
-
 # what's the difference between the strings, how would you go back?
-
 # how many different ways can it be changed?
-
 # permutations of the string?
-
 # how many of certain substrings are in the string
-
 # probability
-
 # maybe visualize it?
-
 # shiny app?
 # with buttons for selecting species,
-
 # combinat::permn
 
 library(combinat)
 library(tidyverse)
 
-species <- c(
-  "ATGCTTAGC",
-  "GTACCCAAT",
-  "GTACGTAAT",
-  "GTATAATA",
-  "GTACC",
-  "TACGGTA",
-  "GATCC",
-  "ATCG",
-  "ATTTGCA",
-  "ATCG"
-)
-
-stresses <- c(
-  "heat",
-  "light",
-  "light",
-  "food",
-  "heat",
-  "light",
-  "food",
-  NA,
-  "light",
-  "heat"
-)
-
 find_matches <- function(specie, stress = NA) {
-  
-  #for ()
+
+  # for ()
   if (!is.na(stress)) {
-    
+
     # set seed for sampling - do we even need this?
     if (stress == "heat") {
       set.seed(34524)
@@ -68,130 +30,35 @@ find_matches <- function(specie, stress = NA) {
     if (stress == "light") {
       set.seed(5432)
     }
-    
+
     if (stress == "food") {
       set.seed(65312)
     }
 
-    # try getting a new permutation
-    #specie_split <- strsplit(specie, "")[[1]]
-    
-    # for (i in specie_split) {
-    #  new <- str_replace(specie_split, specie_split, sample(c("A", "T", "G", "C"), 1))
-    # }
-    
     # we are assuming each nucleotide represents a substring, so really, it is moving around substrings
     # we can see how many remain in the same position?
     old_split <- strsplit(specie, "")[[1]]
     new <- sample(permn(old_split), 1) %>%
       unlist() %>%
       paste(collapse = "")
-    # split into nucleotides  
+    # split into nucleotides
     new_split <- strsplit(new, "")[[1]]
     # find the position of the matches
     i <- which(old_split == new_split)
-    if (length(i) == 0L) {i <- NA }
+    if (length(i) == 0L) {
+      i <- NA
+    }
   }
-  
+
   # if na, no change
   if (is.na(stress)) {
     new <- specie
     i <- NA
-    }
+  }
 
-  
+
   # form output df
   out <- data.frame(specie, new, stress, i)
-  
+
   return(out)
 }
-
-# # creates a bound df together
-# 
-# find_matches(species[[1]], stresses[[1]])
-# find_matches(species[[2]], stresses[[2]])
-# find_matches(species[[3]], stresses[[3]])
-# find_matches(species[[4]], stresses[[4]])
-# 
-# find_matches("GTACGTAATA", "food")
-
-# max length is 10
-
-# stress = "light"
-df <- map2_dfr(species, stresses, find_matches)
-# # 
-# # 
-summary <- df %>% group_by(specie, stress) %>%
-  summarise(count_changes = n()) %>%
-  mutate(total_nucleotides = nchar(specie),
-         prob_changes = count_changes/total_nucleotides) %>%
-  arrange(desc(prob_changes))
-#   
-  #df %>% pivot_wider(values_from = i, names_repair = "unique")
-
-# bargraph
-ggplot(summary, aes(x=prob_changes)) +
-  geom_boxplot()
-
-# box plot
-ggplot(summary, aes(x=specie, y=prob_changes)) +
-  geom_col() + coord_flip()
-
-
-
-
-# try same specie with different stresses
-df <- map2_dfr("GTACGTAATA", c("food", "heat", "light"), find_matches)
-summary <- df %>% group_by(specie, stress) %>%
-  summarise(count_changes = n()) %>%
-  mutate(total_nucleotides = nchar(specie),
-         prob_changes = count_changes/total_nucleotides) %>%
-  arrange(desc(prob_changes))
-
-ggplot(summary, aes(x=prob_changes)) +
-  geom_boxplot()
-#find_matches("GTACGTAATA", c("food", "heat", "light"))
-
-# test all with all stresses
-
-df2 <- map2_dfr(species, c("food", "heat", "light"), find_matches)
-total_df <- tibble(specie = character(),
-                   new = character(), 
-                   stress = character(),
-                   i = integer()
-                   )
-
-for (specie in species) {
-  total_df <- map2_dfr(specie, c("food", "heat", "light"), find_matches) %>% bind_rows(total_df)
-}
-summary <- total_df %>% group_by(specie, stress) %>%
-  summarise(count_changes = n()) %>%
-  mutate(total_nucleotides = nchar(specie),
-         prob_changes = count_changes/total_nucleotides) %>%
-  arrange(desc(prob_changes))
-
-ggplot(summary, aes(x=prob_changes)) +
-  geom_boxplot() + facet_wrap(~stress)
-
-ggplot(summary, aes(x=specie, y=prob_changes, fill = stress)) +
-  geom_col(position = "dodge") + coord_flip()
-
-# 
-# # maybe print both out and visualize it?
-# 
-# # calculate the probability of which stresses, and to which anemone change the the string the most
-# # if 
-# 
-# 
-# # ideally would take in and randomly replace string
-# 
-# # so func 1, is a simulation of what the lab would give dna trackers
-# 
-# # func 2 finds match indices
-# 
-# # func 3 calculates probability of some stresses affecting a set of anemones and 
-# # seeing how many changes occur - a bar graph perhaps
-# 
-# prob <- function(df) {
-#   # df has columns
-# }
